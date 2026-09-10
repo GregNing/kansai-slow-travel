90b7fe467249a23fdbe5b8c984014b7a04b8b03a
GregNing <gregjiening@gmail.com>
2026-09-10 17:49:11 +0800

    Update return route and refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kansai-slow-travel-v7';
-const ASSETS = ['./', './index.html', './styles.css', './data.js', './app.js', './manifest.webmanifest'];
+const CACHE = 'kansai-slow-travel-v9';
+const ASSETS = ['./', './index.html', './styles.css', './data.js?v=9', './app.js?v=9', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
