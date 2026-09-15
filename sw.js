e974f76e82bd61c2deb93dca37152fa6f6074d0b
GregNing <gregjiening@gmail.com>
2026-09-15 16:05:33 +0800

    加入百鬼夜行與更新旅程憑證

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kansai-slow-travel-v43';
-const ASSETS = ['./', './index.html', './styles.css', './data.js?v=43', './app.js?v=43', './manifest.webmanifest'];
+const CACHE = 'kansai-slow-travel-v49';
+const ASSETS = ['./', './index.html', './data.js?v=49', './app.js?v=49', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
