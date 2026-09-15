cd62e439dd6135f52ccf890ee01e749840c9ad33
GregNing <gregjiening@gmail.com>
2026-09-15 11:29:46 +0800

    修正手機版分帳表滑動

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kansai-slow-travel-v42';
-const ASSETS = ['./', './index.html', './styles.css', './data.js?v=42', './app.js?v=42', './manifest.webmanifest'];
+const CACHE = 'kansai-slow-travel-v43';
+const ASSETS = ['./', './index.html', './styles.css', './data.js?v=43', './app.js?v=43', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
