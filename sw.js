c4a66ca09dd5878a8a5f90dc3ca1dd18e74ee973
GregNing <gregjiening@gmail.com>
2026-09-21 16:12:20 +0800

    Clarify Hozugawa shuttle access

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kansai-slow-travel-v63';
-const ASSETS = ['./', './index.html', './styles.css?v=63', './data.js?v=63', './app.js?v=63', './manifest.webmanifest'];
+const CACHE = 'kansai-slow-travel-v67';
+const ASSETS = ['./', './index.html', './styles.css?v=67', './data.js?v=67', './app.js?v=67', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
