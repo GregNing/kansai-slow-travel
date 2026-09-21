027e375a9723dd824962881640a9bd85043079e4
GregNing <gregjiening@gmail.com>
2026-09-21 16:22:14 +0800

    Use confirmed Hozugawa shuttle map

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kansai-slow-travel-v67';
-const ASSETS = ['./', './index.html', './styles.css?v=67', './data.js?v=67', './app.js?v=67', './manifest.webmanifest'];
+const CACHE = 'kansai-slow-travel-v68';
+const ASSETS = ['./', './index.html', './styles.css?v=68', './data.js?v=68', './app.js?v=68', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
